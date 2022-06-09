88cb094bc23d5c963b5ad4d09fa52c8db2e19d1d
rkipchumba <98005951+rkipchumba@users.noreply.github.com>
2022-06-09 15:42:05 +0300

    added store dict

--- app.py
+++ app.py
@@ -1,9 +1,48 @@
 from flask import Flask 
 
 app = Flask(__name__)
+stores = [
+    {
+        'name': 'My Wonderful Store',
+        'items': [
+            {
+            'name':'my item', 
+            'price': 15.99 
+            }
+        ]
+    }
+]
 
-@app.route("/")
-def home():
-    return "Hello,World!"
+
+#POST - used to receive data
+#GET - used to send data back only
+
+#post /store data: {name :}
+@app.route("/store", methods=["POST"])
+def create_store():
+    pass
+
+#get /store/<name> data: {name :}
+@app.route("/store/<string:name>")
+def get_store(name):
+    pass
+
+#get /store
+@app.route("/store")
+def get_stores():
+    pass
+
+
+#POST /store/<name> data: {name :}
+@app.route("/store/<string:name>/item", methods=["POST"])
+def create_item_in_store(name):
+    pass
+
+
+#get /store/<name>/item data: {name :}
+@app.route("/store/<string:name>/item")
+def get_item_in_store():
+    pass
+
 
 app.run(port=5000)
